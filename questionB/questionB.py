2bf8d101156e3389f8933cc06d680a88bfe8777d
Bebo Elhosary <beboelhosary@hotmail.com>
2018-09-14 13:45:03 -0400

    Clean comparable function

--- questionB/questionB.py
+++ questionB/questionB.py
@@ -3,9 +3,9 @@ from fractions import Fraction
 
 """
 def comparable(str1, str2):
-	
-	# Filter bools
-	if (str1 == True) or (str2 == True) or (str1 == False) or (str2 == False):
+
+	# Filter out bools
+	if isinstance(str1, bool) or isinstance(str2, bool):
 		return False
 
 	# If either string is unconvertible to float,
